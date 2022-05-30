c36c91d35c612e2980364ecdbea24e7b5012fb52
Anton Thomasson <antonthomasson@gmail.com>
2022-05-30 19:50:54 +0200

    C++-ify baselinify a bit more

--- baselinify.cpp
+++ baselinify.cpp
@@ -2,7 +2,7 @@
 #include <jpeglib.h>
 #include "madness.h"
 
-struct bts_source_mgr
+struct bts_source_mgr: jpeg_source_mgr
 {
   bts_source_mgr(Bytestream& in) : bts(in)
   {
@@ -11,18 +11,13 @@ struct bts_source_mgr
     FUNC(jpeg, boolean, jpeg_resync_to_restart, j_decompress_ptr, int);
     #endif
 
-    pub.init_source = init_source;
-    pub.fill_input_buffer = fill_input_buffer;
-    pub.skip_input_data = skip_input_data;
-    pub.resync_to_restart = jpeg_resync_to_restart;
-    pub.term_source = term_source;
-    pub.bytes_in_buffer = (size_t)bts.size();
-    pub.next_input_byte = (const JOCTET *)bts.raw();
-  }
-
-  operator jpeg_source_mgr*()
-  {
-    return (jpeg_source_mgr*)this;
+    jpeg_source_mgr::init_source = init_source;
+    jpeg_source_mgr::fill_input_buffer = fill_input_buffer;
+    jpeg_source_mgr::skip_input_data = skip_input_data;
+    jpeg_source_mgr::resync_to_restart = jpeg_resync_to_restart;
+    jpeg_source_mgr::term_source = term_source;
+    jpeg_source_mgr::bytes_in_buffer = (size_t)bts.size();
+    jpeg_source_mgr::next_input_byte = (const JOCTET *)bts.raw();
   }
 
   static void init_source(j_decompress_ptr) {}
@@ -35,30 +30,24 @@ struct bts_source_mgr
   static void skip_input_data(j_decompress_ptr cinfo, long num_bytes)
   {
     bts_source_mgr* src = (bts_source_mgr*)cinfo->src;
-    src->pub.next_input_byte += (size_t)num_bytes;
-    src->pub.bytes_in_buffer -= (size_t)num_bytes;
+    src->next_input_byte += (size_t)num_bytes;
+    src->bytes_in_buffer -= (size_t)num_bytes;
   }
 
   static void term_source(j_decompress_ptr) {}
 
-  struct jpeg_source_mgr pub;
   Bytestream& bts;
 };
 
-struct bts_destination_mgr
+struct bts_destination_mgr: jpeg_destination_mgr
 {
   bts_destination_mgr(Bytestream& out) : bts(out)
   {
-    pub.init_destination = init_destination;
-    pub.empty_output_buffer = empty_output_buffer;
-    pub.term_destination = term_destination;
-    pub.next_output_byte = buffer;
-    pub.free_in_buffer = BS_REASONABLE_FILE_SIZE;
-  }
-
-  operator jpeg_destination_mgr*()
-  {
-    return (jpeg_destination_mgr*)this;
+    jpeg_destination_mgr::init_destination = init_destination;
+    jpeg_destination_mgr::empty_output_buffer = empty_output_buffer;
+    jpeg_destination_mgr::term_destination = term_destination;
+    jpeg_destination_mgr::next_output_byte = buffer;
+    jpeg_destination_mgr::free_in_buffer = BS_REASONABLE_FILE_SIZE;
   }
 
   static void init_destination(j_compress_ptr) {}
@@ -68,19 +57,18 @@ struct bts_destination_mgr
     bts_destination_mgr* dest = (bts_destination_mgr*)cinfo->dest;
     dest->bts.putBytes(dest->buffer, BS_REASONABLE_FILE_SIZE);
 
-    dest->pub.next_output_byte = dest->buffer;
-    dest->pub.free_in_buffer = BS_REASONABLE_FILE_SIZE;
+    dest->next_output_byte = dest->buffer;
+    dest->free_in_buffer = BS_REASONABLE_FILE_SIZE;
     return TRUE;
   }
 
   static void term_destination(j_compress_ptr cinfo)
   {
     bts_destination_mgr* dest = (bts_destination_mgr*)cinfo->dest;
-    size_t size = (BS_REASONABLE_FILE_SIZE - dest->pub.free_in_buffer);
+    size_t size = (BS_REASONABLE_FILE_SIZE - dest->free_in_buffer);
     dest->bts.putBytes(dest->buffer, size);
   }
 
-  struct jpeg_destination_mgr pub;
   JOCTET buffer[BS_REASONABLE_FILE_SIZE];
   Bytestream& bts;
 };
@@ -103,7 +91,7 @@ void baselinify(Bytestream& InBts, Bytestream& OutBts)
   jpeg_create_compress(&dstinfo);
 
   bts_source_mgr srcmgr(InBts);
-  srcinfo.src = srcmgr;
+  srcinfo.src = &srcmgr;
 
   // TODO: consider copying or inserting exif header if applicable
 
@@ -112,7 +100,7 @@ void baselinify(Bytestream& InBts, Bytestream& OutBts)
   jpeg_copy_critical_parameters(&srcinfo, &dstinfo);
 
   bts_destination_mgr dstmgr(OutBts);
-  dstinfo.dest = dstmgr;
+  dstinfo.dest = &dstmgr;
 
   jpeg_write_coefficients(&dstinfo, coef_arrays);
   jpeg_finish_compress(&dstinfo);

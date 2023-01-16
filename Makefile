acc32f3769c3b18bd6b94c55b882c7b7b778854c
Anton Thomasson <antonthomasson@gmail.com>
2023-01-16 20:16:55 +0100

    Build PrintParameters implementation only once
    (it's gotten big...)

--- Makefile
+++ Makefile
@@ -15,16 +15,16 @@ baselinify_mad.o: baselinify.cpp
 %.o: %.cpp
 	$(CXX) -c $(CXXFLAGS) $<
 
-ppm2pwg: bytestream.o ppm2pwg.o ppm2pwg_main.o
+ppm2pwg: bytestream.o printparameters.o ppm2pwg.o ppm2pwg_main.o
 	$(CXX) $^ -o $@
 
 pwg2ppm: bytestream.o pwg2ppm.o pwg2ppm_main.o
 	$(CXX) $^ -o $@
 
-pdf2printable: bytestream.o ppm2pwg.o pdf2printable.o pdf2printable_main.o
+pdf2printable: bytestream.o printparameters.o ppm2pwg.o pdf2printable.o pdf2printable_main.o
 	$(CXX) $^ $(shell pkg-config --libs poppler-glib) -o $@
 
-pdf2printable_mad: bytestream.o ppm2pwg.o pdf2printable_mad.o pdf2printable_main.o
+pdf2printable_mad: bytestream.o printparameters.o ppm2pwg.o pdf2printable_mad.o pdf2printable_main.o
 	$(CXX) $^ $(shell pkg-config --libs gobject-2.0) -ldl -o $@
 
 hexdump: bytestream.o hexdump.o
--- printparameters.h
+++ printparameters.h
@@ -2,12 +2,8 @@
 #define PRINTPARAMETERS_H
 
 #include <cmath>
-#include <string>
 #include <vector>
-#include <regex>
-
-#define MM_PER_IN 25.4
-#define PTS_PER_IN 72.0
+#include <string>
 
 #define INVALID_PAGE 0
 
@@ -66,294 +62,27 @@ public:
 
   PageRangeList pageRangeList;
 
-  size_t getPaperSizeWInPixels() const
-  {
-    switch(paperSizeUnits)
-    {
-      case Pixels:
-        return paperSizeW;
-      case Millimeters:
-        return round(paperSizeW * hwResW / MM_PER_IN);
-      case Inches:
-        return paperSizeW * hwResW;
-    }
-    return 0;
-  }
+  size_t getPaperSizeWInPixels() const;
+  size_t getPaperSizeHInPixels() const;
 
-  size_t getPaperSizeHInPixels() const
-  {
-    switch(paperSizeUnits)
-    {
-      case Pixels:
-        return paperSizeH;
-      case Millimeters:
-        return round(paperSizeH * hwResH / MM_PER_IN);
-      case Inches:
-        return paperSizeH * hwResH;
-    }
-    return 0;
-  }
+  size_t getPaperSizeInPixels() const;
 
-  size_t getPaperSizeInPixels() const
-  {
-    return getPaperSizeWInPixels() * getPaperSizeHInPixels();
-  }
+  float getPaperSizeWInMillimeters() const;
+  float getPaperSizeHInMillimeters() const;
 
-  float getPaperSizeWInMillimeters() const
-  {
-    switch(paperSizeUnits)
-    {
-      case Pixels:
-        return round2((paperSizeW / hwResW) * MM_PER_IN);
-      case Millimeters:
-        return paperSizeW;
-      case Inches:
-        return round2(paperSizeW * MM_PER_IN);
-    }
-    return 0;
-  }
+  float getPaperSizeWInPoints() const;
+  float getPaperSizeHInPoints() const;
 
-  float getPaperSizeHInMillimeters() const
-  {
-    switch(paperSizeUnits)
-    {
-      case Pixels:
-        return round2((paperSizeH / hwResH) * MM_PER_IN);
-      case Millimeters:
-        return paperSizeH;
-      case Inches:
-        return round2(paperSizeH * MM_PER_IN);
-    }
-    return 0;
-  }
+  size_t getPaperSizeWInBytes() const;
+  size_t getPaperSizeInBytes() const;
 
-  float getPaperSizeWInPoints() const
-  {
-    switch(paperSizeUnits)
-    {
-      case Pixels:
-        return round2((paperSizeW / hwResW) * PTS_PER_IN);
-      case Millimeters:
-        return round2((paperSizeW / MM_PER_IN) * PTS_PER_IN);
-      case Inches:
-        return paperSizeW * PTS_PER_IN;
-    }
-    return 0;
-  }
+  PageSequence getPageSequence(size_t pages) const;
 
-  float getPaperSizeHInPoints() const
-  {
-    switch(paperSizeUnits)
-    {
-      case Pixels:
-        return round2((paperSizeH / hwResH) * PTS_PER_IN);
-      case Millimeters:
-        return round2((paperSizeH / MM_PER_IN) * PTS_PER_IN);
-      case Inches:
-        return paperSizeH * PTS_PER_IN;
-    }
-    return 0;
-  }
+  bool setPageRange(const std::string& rangeStr);
 
-  size_t getPaperSizeWInBytes() const
-  {
-    if(bitsPerColor == 1)
-    {
-      // Round up to whole bytes
-      return colors * ((getPaperSizeWInPixels() + 7) / 8);
-    }
-    else
-    {
-      return colors * (getPaperSizeWInPixels() / (8 / bitsPerColor));
-    }
-  }
+  bool setPaperSize(const std::string& sizeStr);
 
-  size_t getPaperSizeInBytes() const
-  {
-    return getPaperSizeWInBytes() * getPaperSizeHInPixels();
-  }
-
-  PageSequence getPageSequence(size_t pages) const
-  {
-    PageRangeList tmp = pageRangeList.empty() ? PageRangeList {{0, 0}}
-                                              : pageRangeList;
-    PageSequence seq;
-
-    if(tmp.size() == 1)
-    {
-      std::pair<size_t, size_t> pair = tmp[0];
-      if(pair.first == 0)
-      {
-        pair.first = 1;
-      }
-      if(pair.second == 0)
-      {
-        pair.second = pages;
-      }
-      tmp = {pair};
-    }
-
-    for(const std::pair<size_t, size_t>& r : tmp)
-    {
-      for(size_t p = r.first; p <= std::min(r.second, pages); p++)
-      {
-        seq.push_back(p);
-      }
-    }
-    if(duplex && (documentCopies > 1 || pageCopies > 1) && ((seq.size() % 2) == 1))
-    {
-      seq.push_back(INVALID_PAGE);
-    }
-
-    if(pageCopies > 1)
-    {
-      PageSequence copy;
-      for(PageSequence::iterator it = seq.begin(); it != seq.end(); it++)
-      {
-        if(duplex)
-        {
-          for(size_t pc = pageCopies; pc > 0; pc--)
-          {
-            copy.push_back(*it);
-            copy.push_back(*(it+1));
-          }
-          it++;
-        }
-        else
-        {
-          for(size_t pc = pageCopies; pc > 0; pc--)
-          {
-            copy.push_back(*it);
-          }
-        }
-      }
-      seq = copy;
-    }
-
-    if(documentCopies > 1)
-    {
-      PageSequence copy = seq;
-      for(size_t dc = documentCopies; dc > 1; dc--)
-      {
-        seq.insert(seq.end(), copy.begin(), copy.end());
-      }
-    }
-    return seq;
-  }
-
-  bool setPageRange(const std::string& rangeStr)
-  {
-    PageRangeList rangeList;
-    const std::regex single("^([0-9]+)$");
-    const std::regex range("^([0-9]+)-([0-9]+)$");
-    std::smatch match;
-
-    size_t pos = 0;
-    while(pos <= rangeStr.length())
-    {
-      size_t found = std::min(rangeStr.length(), rangeStr.find(",", pos));
-      std::string tmp(rangeStr, pos, (found-pos));
-
-      if(std::regex_match(tmp, match, single))
-      {
-        size_t tmp = stol(match[1]);
-        rangeList.push_back({tmp, tmp});
-      }
-      else if(std::regex_match(tmp, match, range))
-      {
-        size_t tmp_from = stol(match[1]);
-        size_t tmp_to = stol(match[2]);
-        if(tmp_to < tmp_from)
-        {
-          return false;
-        }
-        rangeList.push_back({tmp_from, tmp_to});
-      }
-      else
-      {
-        return false;
-      }
-      pos = found+1;
-    }
-
-    if(!rangeList.empty())
-    {
-      pageRangeList = rangeList;
-      return true;
-    }
-    return false;
-  }
-
-  bool setPaperSize(const std::string& sizeStr)
-  {
-    const std::regex nameRegex("^[0-9a-z_-]+_(([0-9]+[.])?[0-9]+)x(([0-9]+[.])?[0-9]+)(mm|in)$");
-    std::cmatch match;
-    locale_t c_locale = newlocale(LC_ALL_MASK, "C", NULL);
-
-    if(std::regex_match(sizeStr.c_str(), match, nameRegex))
-    {
-      paperSizeName = sizeStr;
-      paperSizeW = strtof_l(match[1].first, nullptr, c_locale);
-      paperSizeH = strtof_l(match[3].first, nullptr, c_locale);
-      if(match[5] == "in")
-      {
-        paperSizeUnits = Inches;
-      }
-      else
-      {
-        paperSizeUnits = Millimeters;
-      }
-      return true;
-    }
-    return false;
-  }
-
-  bool setColorMode(std::string colorMode)
-  {
-    if(colorMode == "srgb24")
-    {
-      colors = 3;
-      bitsPerColor = 8;
-      black = false;
-      return true;
-    }
-    else if(colorMode == "cmyk32")
-    {
-      colors = 4;
-      bitsPerColor = 8;
-      black = false;
-      return true;
-    }
-    else if(colorMode == "gray8")
-    {
-      colors = 1;
-      bitsPerColor = 8;
-      black = false;
-      return true;
-    }
-    else if(colorMode == "black8")
-    {
-      colors = 1;
-      bitsPerColor = 8;
-      black = true;
-      return true;
-    }
-    else if(colorMode == "gray1")
-    {
-      colors = 1;
-      bitsPerColor = 1;
-      black = false;
-      return true;
-    }
-    else if(colorMode == "black1")
-    {
-      colors = 1;
-      bitsPerColor = 1;
-      black = true;
-      return true;
-    }
-    return false;
-  }
+  bool setColorMode(std::string colorMode);
 
 private:
 

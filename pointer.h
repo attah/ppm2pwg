1f6f5e3afec3112ad614bc794b052ecdd54ad725
attah <antonthomasson@gmail.com>
2023-04-03 19:57:44 +0200

    Add pointer abstraction (#8)

---
+++ pointer.h
@@ -0,0 +1,25 @@
+#ifndef POINTER_H
+#define POINTER_H
+
+#include <memory>
+#include <functional>
+
+template<typename T, typename D = std::function<void(T*)>>
+class Pointer : public std::unique_ptr<T, D>
+{
+public:
+  Pointer(T* ptr, D deleter)
+  : std::unique_ptr<T, D>(ptr, [deleter](T* ptr){if(ptr) deleter(ptr);})
+  {}
+  Pointer<T, D>& operator=(T* Ptr)
+  {
+    std::unique_ptr<T, D>::reset(Ptr);
+    return *this;
+  }
+  operator T*() const
+  {
+    return std::unique_ptr<T, D>::get();
+  }
+};
+
+#endif //POINTER_H
